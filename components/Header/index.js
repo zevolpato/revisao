8fc6481d05c8b43c43104d92bcf3ff4f0cb3a41a
jose.avjunior <jose.avjunior@sp.senac.br>
2022-10-06 12:08:16 -0300

    first

--- components/Header/index.js
+++ components/Header/index.js
@@ -1,9 +1,14 @@
+/* eslint-disable jsx-a11y/alt-text */
 import styles from './styles.module.css'
+import logo from  '../../public/img.png'
+import Image from 'next/future/image'
 
 function Header() {
     return(
         <div className={styles.menu}>
-        <p>Header teste</p>
+
+     <Image src= {logo} ></Image>
+        
         </div>
     )
     
